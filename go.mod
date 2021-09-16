0b9df278c34c7ea404a6b36f5570c16267b0844f
sonmaximum <maxjuchheim@gmail.com>
2021-09-16 11:19:09 +0900

    add package examples

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example/user/gocode
 
 go 1.17
+
+require github.com/google/go-cmp v0.5.6
--- gocode.go
+++ gocode.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"example/user/gocode/morestrings"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println("Hello, worlds.")
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
